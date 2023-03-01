381fb08bbc65e817090ce0324b8ba28c87966e4e
Carlos Daniel Martínez López <carlos.martinez.cbtis59@gmail.com>
2023-03-01 14:12:05 -0600

    login completamente funcional

--- php/loginController.php
+++ php/loginController.php
@@ -3,20 +3,30 @@
     require 'db_connection.php';
     $datos=array();//guardar informacion del usuario
 
-    $tsql = "SELECT * FROM tblLogin WHERE Usuario = ? AND Contrasena = ?";
-    $params = array($_POST['usuario'], $_POST['contraseña']);
-
-    $stmt = sqlsrv_query($connection, $tsql, $params);
-
-    if($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)){ //validamos si se encontro el registro
-            $datos['status']=1;
+    if(empty($_POST['usuario']) || empty($_POST['contraseña'])){
+        // Uno o ambos campos están vacíos, muestra un mensaje de error
+        $response = array('status' => 0, 'msg' => 'Por favor, complete AMBOS campos.');
+        echo json_encode($response);
     }
-    else{ //accion si no se encuentra el registro
-        $datos['status']=0;
-        $datos['msg']="Usuario o contraseña no valida";
+    else{
+        // Ambos campos están completos, continua con la lógica de tu programa
+        // Aquí puedes agregar tu código para verificar si el usuario y la contraseña son válidos
+
+        $tsql = "SELECT * FROM tblLogin WHERE Usuario = ? AND Contrasena = ?";
+        $params = array($_POST['usuario'], $_POST['contraseña']);
+
+        $stmt = sqlsrv_query($connection, $tsql, $params);
+
+        if($row = sqlsrv_fetch_array($stmt, SQLSRV_FETCH_ASSOC)){ //validamos si se encontro el registro
+                $datos['status']=1;
+        }
+        else{ //accion si no se encuentra el registro
+            $datos['status']=0;
+            $datos['msg']="Usuario o contraseña no valida";
+        }
+        sqlsrv_close($connection);
+        //convertir array a json
+        $json=json_encode($datos);
+        echo $json;
     }
-    sqlsrv_close($connection);
-    //convertir array a json
-    $json=json_encode($datos);
-    echo $json;
 ?>
