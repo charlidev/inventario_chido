279cc803c840eb9285099f17e4946653ad664f21
charlidev <carlos.martinez.cbtis59@gmail.com>
2023-03-20 23:41:33 -0600

    empezando con la tabla de dependencias

--- sidebar.php
+++ sidebar.php
@@ -2,7 +2,7 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="home.php">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
@@ -24,7 +24,7 @@
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <a class="collapse-item" href="buttons.html">Dependencias</a>
+                        <a class="collapse-item" href="dependencies.php">Dependencias</a>
                         <a class="collapse-item" href="cards.html">Roles</a>
                     </div>
                 </div>
