a308c2aef7c41e9a1538c799093604e1b7a4edde
Carlos Daniel Martínez López <carlos.martinez.cbtis59@gmail.com>
2023-03-21 15:22:36 -0600

    datatable integrada sin conexion a bd

--- php/db_connection.php
+++ php/db_connection.php
@@ -2,7 +2,7 @@
 //Nombre del servidor de la base de datos
 //Server PC: DESKTOP-RFI1D63
 //Server Lap: DESKTOP-GBQUMCE
-$serverName = "DESKTOP-RFI1D63";
+$serverName = "DESKTOP-GBQUMCE";
 //Información de la base de datos
 $connectionInfo = array("Database"=>"Sistema_Inventario", "UID"=>"sa", "PWD"=>"12345");
 //Se establece la conexión a la base de datos
