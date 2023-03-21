a9a39727b74f1323efc78235dfbf2100b219f242
Carlos Daniel Martínez López <carlos.martinez.cbtis59@gmail.com>
2023-03-21 17:02:25 -0600

    modal funcionando sin formulario

--- dependencies.php
+++ dependencies.php
@@ -34,26 +34,24 @@
                     <!-- Page Heading -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">Catálogo de Dependencias</h1>
-                        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
-                        <i class="text-white-50"></i>  Agregar Dependencia</a>
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarDependencia" id="btnAgregarDependencia">
+                            Agregar Dependencia
+                        </button>
                     </div>
 
                     <!-- Datatable -->
                     <div class="card shadow mb-4">
                         <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                            <h6 class="m-0 font-weight-bold text-primary">Aquí podrían ir los botones para exportar.</h6>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
                                 <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                                     <thead>
                                         <tr>
-                                            <th>Name</th>
-                                            <th>Position</th>
-                                            <th>Office</th>
-                                            <th>Age</th>
-                                            <th>Start date</th>
-                                            <th>Salary</th>
+                                            <th>Dependencia</th>
+                                            <th>Activa</th>
+                                            <th>Acciones</th>
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -61,25 +59,16 @@
                                             <td>Tiger Nixon</td>
                                             <td>System Architect</td>
                                             <td>Edinburgh</td>
-                                            <td>61</td>
-                                            <td>2011/04/25</td>
-                                            <td>$320,800</td>
                                         </tr>
                                         <tr>
                                             <td>Garrett Winters</td>
                                             <td>Accountant</td>
                                             <td>Tokyo</td>
-                                            <td>63</td>
-                                            <td>2011/07/25</td>
-                                            <td>$170,750</td>
                                         </tr>
                                         <tr>
                                             <td>Ashton Cox</td>
                                             <td>Junior Technical Author</td>
                                             <td>San Francisco</td>
-                                            <td>66</td>
-                                            <td>2009/01/12</td>
-                                            <td>$86,000</td>
                                         </tr>
                                     </tbody>
                                 </table>
@@ -89,10 +78,11 @@
                     <!-- Termina el Datatable -->
                 </div>
                 <!-- Termina contenido de página -->
+                
+
 
             </div>
             <!-- End of Main Content -->
-
             <!-- Footer -->
             <?php require('footer.php')?>
             <!-- End of Footer -->
@@ -103,4 +93,25 @@
     </div>
     <!-- End of Page Wrapper -->
 
+
+    <!-- Aqui puede ir el codigo del modal -->
+    <div class="modal fade" id="modalAgregarDependencia" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            <div class="modal-body">
+                ...
+            </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
