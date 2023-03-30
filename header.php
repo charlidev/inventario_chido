47951cbd9722fa3715350459b277b8ae16250358
Carlos Daniel Martínez López <carlos.martinez.cbtis59@gmail.com>
2023-03-30 17:01:57 -0600

    ya exporta a pdf y svg

--- header.php
+++ header.php
@@ -20,6 +20,7 @@
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
     <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    <link href="vendor/datatables/buttons.bootstrap4.min.css" rel="stylesheet">
 
 </head>
 
@@ -35,7 +36,20 @@
 
 <!-- Page level plugins -->
 <script src="vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+<script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+<script src="vendor/datatables/jszip.min.js"></script>
+<script src="vendor/datatables/pdfmake.min.js"></script>
+<script src="vendor/datatables/vfs_fonts.js"></script>
+<script src="vendor/datatables/dataTables.buttons.min.js"></script>
+<script src="vendor/datatables/buttons.bootstrap4.min.js"></script>
+<script src="vendor/datatables/buttons.colVis.min.js"></script>
+<script src="vendor/datatables/buttons.html5.min.js"></script>
+<script src="vendor/datatables/buttons.print.min.js"></script>
+
+
+
+
+
 
 <!-- Page level custom scripts -->
 <script src="js/demo/datatables-demo.js"></script>
--- js/demo/datatables-demo.js
+++ js/demo/datatables-demo.js
@@ -1,4 +1,25 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#dataTable').DataTable();
+  $('#dataTable').DataTable({
+    // aquí se modifica la cantidad de registros a mostrar en la tabla
+    lengthMenu: [10, 20, 30, 40, 50],
+    columnDefs: [
+      // aquí se define por cuales columnas NO hace la busqueda
+      {searchable:false, target:[1,2]}
+    ],
+    dom:"Bfrtilp",
+    buttons: [{
+      extend: 'excelHtml5',
+      text: "<i class='fas fa-file-csv'></i>",
+      titleAttr: 'Exportar archivo Excel',
+      className: 'btn btn-success'
+      },
+      {
+        extend: 'pdfHtml5',
+        text: "<i class='fas fa-file-pdf'></i>",
+        titleAttr: 'Exportar archivo PDF',
+        className: 'btn btn-danger'
+      }
+    ],
+  });
 });
